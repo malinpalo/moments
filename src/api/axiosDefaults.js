5f9acd520545f7ad73f17262966a6554b478ada6
malinpalo <malinpalo.mp@gmail.com>
2023-05-26 08:40:48 +0000

    Refresh

--- src/api/axiosDefaults.js
+++ src/api/axiosDefaults.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-axios.defaults.baseURL = 'https://drf-api-2.herokuapp.com/'
+axios.defaults.baseURL = 'https://drf-api-2.herokuapp.com'
 axios.defaults.headers.post['Content-Type'] = 'multipart/form-data'
 axios.defaults.withCredentials = true
 
